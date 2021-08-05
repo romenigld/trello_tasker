fe68024cf96d4370eb00be5c3aa8706ac8eec513
Romenig Lima Damasio <romenigld@gmail.com>
2021-08-05 16:16:56 +0200

    coloquei padrao POOL_SIZE 2

--- config/releases.exs
+++ config/releases.exs
@@ -10,7 +10,7 @@ database_url =
 config :trello_tasker, TrelloTasker.Repo,
   # ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
