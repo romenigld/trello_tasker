b59582e5e7a3ab9dec14ba3596683c226737aeb2
Romenig Lima Damasio <romenigld@gmail.com>
2021-08-13 19:28:35 +0200

    trello tasker

---
+++ lib/trello_tasker/shared/services/trello.ex
@@ -0,0 +1,45 @@
+defmodule TrelloTasker.Shared.Services.Trello do
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "https://api.trello.com/1/cards"
+  plug Tesla.Middleware.Headers, [{"User-Agent", "request"}]
+  plug Tesla.Middleware.JSON
+
+  @token Application.get_env(:trello_tasker,  :trello)[:token]
+  @key Application.get_env(:trello_tasker,  :trello)[:key]
+
+  def get_comments(card_id) do
+    {:ok, response} =
+      ("#{card_id}/actions?commentCard&key=" <> @key <> "&token=" <> @token)
+      |> get()
+
+    body = response.body
+
+    body
+    |> Enum.map(&%{text: &1["data"]["text"], user: &1["memberCreator"]["username"]})
+  end
+
+  def get_card(card_id) do
+    {:ok, response} = "#{card_id}?list=true&key=" <> @key <> "&token=" <> @token
+    |> get()
+
+    status = response.status
+
+    if status != 200 do
+      {:error, "Erro ao buscar o card"}
+    else
+      body = response.body
+
+      {:ok, deliver_date, _} = body["due"] |> DateTime.from_iso8601()
+
+      %{
+        name: body["name"],
+        description: body["desc"],
+        image: body["cover"]["sharedSourceUrl"],
+        card_id: body["shortLink"],
+        completed: body["dueComplete"],
+        deliver_date: deliver_date |> DateTime.to_date()
+      }
+    end
+  end
+end
--- lib/trello_tasker_web/live/card_live.ex
+++ lib/trello_tasker_web/live/card_live.ex
@@ -3,13 +3,60 @@ defmodule TrelloTaskerWeb.CardLive do
 
   alias Phoenix.View
   alias TrelloTaskerWeb.CardView
+  alias TrelloTasker.Cards
+  alias TrelloTasker.Cards.Card
+  alias TrelloTasker.Shared.Services.Trello
 
   @impl true
   def mount(_params, _session, socket) do
+    changeset =
+      Card.changeset(%Card{})
+    card =
+      Trello.get_card("bjNYC1Pt")
+    cards =
+      Cards.list_cards()
+      |> Enum.map(&Trello.get_card(&1.path))
+
+    socket =
+      socket
+      |> assign(cards: cards, changeset: changeset)
+
     {:ok, socket}
   end
 
+  @impl true
   def render(assigns) do
     View.render(CardView, "index.html", assigns)
+  end
+
+  @impl true
+  def handle_event("create", %{"card" => card}, socket) do
+    changeset =
+      %Ecto.Changeset{Card.changeset(%Card{}, card) | action: :insert}
+
+      changeset.valid?
+      |> case do
+        false -> {:noreply, assign(socket, changeset: changeset)}
+        true ->
+          card["path"]
+          |> Trello.get_card()
+          |> case do
+            {:error, msg} ->
+              {:noreply, socket |> put_flash(:error, msg) |> push_redirect(to: "/")}
+
+            card_info ->
+              card
+              |> Cards.create_card()
+              |> response(socket)
+          end
+      end
+  end
+
+  defp response({:ok, _card}, socket) do
+    {:noreply, socket |> put_flash(:info, "card criado com sucesso!") |> push_redirect(to: "/")}
+  end
+
+  defp response({:error, changeset}, socket) do
+    {:noreply, assign(socket, changeset: changeset)}
   end
 end
