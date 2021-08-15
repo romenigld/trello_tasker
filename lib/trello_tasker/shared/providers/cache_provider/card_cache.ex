1877b9bca124d73ecde64a3e3886c7d37cf559d4
Romenig Lima Damasio <romenigld@gmail.com>
2021-08-15 13:56:14 +0200

    Introdução ao GenServer

---
+++ lib/trello_tasker/shared/providers/cache_provider/card_cache.ex
@@ -0,0 +1,31 @@
+defmodule TrelloTasker.Shared.Providers.CacheProvider.CardCache do
+  use GenServer
+
+  @db :cards
+
+  def start_link(_), do: GenServer.start_link(__MODULE__, %{}, name: :cards_cache)
+
+  @impl true
+  def init(state) do
+    :ets.new(@db, [:set, :public, :named_table])
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_cast({:put, key, value}, state) do
+    :ets.insert(@db, {key, value})
+    {:noreply, state}
+  end
+
+  def handle_call({:get, key}, _from, state) do
+    response =
+      :ets.lookup(@db, key)
+      |> case do
+        [] -> {:not_found, []}
+        [{_k, value}] -> {:ok, value}
+      end
+
+    {:reply, response, state}
+  end
+end
